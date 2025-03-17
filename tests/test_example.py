72dfe1ec27374bdf2e0c2d2d785b95c668760d44
naste <nastenka1193@mail.ru>
2025-03-17 07:15:49 +0300

    Add allure report

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,38 +2,43 @@ from conftest import form_fields_page
 from selenium.common import UnexpectedAlertPresentException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import allure
 
+@allure.feature('Form Fields')
+@allure.story('Existence alert with message')
 def test_successful_form_fields(form_fields_page):
+    with allure.step('Fill in the Name field'):
+        form_fields_page.fill_name('name')
+    with allure.step('Fill in the Password field'):
+        form_fields_page.fill_password('qwerty')
 
-    #Заполнение полей name и password
-    form_fields_page.fill_name('name')
-    form_fields_page.fill_password('qwerty')
+    with allure.step('Choose Milk and Coffee from the What is your favorite drink list?'):
+        form_fields_page.select_checkbox('Milk')
+        form_fields_page.select_checkbox('Coffee')
 
-    #Выбор Milk и Coffee из списка What is your favorite drink?
-    form_fields_page.select_checkbox('Milk')
-    form_fields_page.select_checkbox('Coffee')
+    with allure.step('Choose Yellow from the What is your favorite color list?'):
+        form_fields_page.select_radio('Yellow')
 
-    # Выбор Yellow из списка What is your favorite color?
-    form_fields_page.select_radio('Yellow')
+    with allure.step('Choose Yes from the Do you like automation list?'):
+        form_fields_page.select_dropdown('yes')
 
-    # Выбор Yes из списка Do you like automation?
-    form_fields_page.select_dropdown('yes')
+    with allure.step('Add the number of tools and the tool with the maximum number of characters'):
+        form_fields_page.add_message()
 
-    #Добавление количества инструментов и инструмента с max количеством символов
-    form_fields_page.add_message()
+    with allure.step('Fill in the Email field'):
+        form_fields_page.fill_email('name@example.com')
 
-    #Заполнение поля email
-    form_fields_page.fill_email('name@example.com')
+    with allure.step('Click Submit button'):
+        try:
+            form_fields_page.submit_form()
+            # Ожидайте появления алерта
+            alert_text = WebDriverWait(form_fields_page.driver, 20).until(
+                EC.alert_is_present()
+            )
+        except UnexpectedAlertPresentException as e:
+            # Обработка исключения
+            alert_text = e.msg.split(": ")[2]  # Извлечение текста алерта из сообщения исключения
+            alert_text = alert_text.split("}")[0]
 
-    try:
-        # Нажать на кнопку Submit
-        form_fields_page.submit_form()
-        # Ожидайте появления алерта
-        alert_text = WebDriverWait(form_fields_page.driver, 20).until(
-            EC.alert_is_present()
-        )
-    except UnexpectedAlertPresentException as e:
-        # Обработка исключения
-        alert_text = e.msg.split(": ")[2]  # Извлечение текста алерта из сообщения исключения
-        alert_text = alert_text.split("}")[0]
-    assert alert_text == "Message received!"
+    with allure.step('Check message'):
+        assert alert_text == "Message received!"
