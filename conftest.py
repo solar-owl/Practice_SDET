8e91c83d3983015268361886c4c4bcb44ca86cf9
naste <nastenka1193@mail.ru>
2025-03-17 05:37:55 +0300

    First action

--- conftest.py
+++ conftest.py
@@ -1,11 +1,14 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from pages.form_fields_page import FormFieldsPage
 
 @pytest.fixture(scope="function")
 def driver():
     # Экземпляр драйвера
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
     #  Перейдем по ссылке https://practice-automation.com/form-fields
     driver.get('https://practice-automation.com/form-fields')
     yield driver
